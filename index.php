5d59786e48db26d90c854cd1274419c41b8217fa
Gislerio Junior <gisleriojunior@hotmail.com>
2022-07-05 01:59:59 -0300

    Abstraindo os atributos e métodos

--- index.php
+++ index.php
@@ -4,5 +4,33 @@ require __DIR__."/vendor/autoload.php";
 
 use App\CarrinhoCompra;
 
+$carrinho1 = new CarrinhoCompra();
+print_r($carrinho1->exibirItens());
 
-echo CarrinhoCompra::teste();
+echo 'valor total:'. $carrinho1->exibirValorTotal();
+
+/* $carrinho1->adicionarItem('Bicicleta', 750.10);
+$carrinho1->adicionarItem('Geladeira', 1950.15);
+$carrinho1->adicionarItem('Tapete', 350.20); */
+
+$carrinho1->adicionarItem('Televisão', 3750.10);
+echo "<br />";
+print_r($carrinho1->exibirItens());
+echo "<br />";
+
+$carrinho1->exibirValorTotal();
+echo 'Valor total recalculado:'. $carrinho1->exibirValorTotal();
+
+echo "<br />";
+echo 'Status:'. $carrinho1->exibirStatus();
+
+
+echo "<br />";
+if($carrinho1->confirmarPedido()){
+    echo 'Pedido realizado com sucesso!';
+} else {
+    echo 'erro na confirmação do pedido. Carrinho vazio';
+}
+
+echo "<br />";
+echo 'Status:'. $carrinho1->exibirStatus();
--- src/CarrinhoCompra.php
+++ src/CarrinhoCompra.php
@@ -2,9 +2,62 @@
 
 namespace App;
 
-class CarrinhoCompra {
-    public static function teste()
+class CarrinhoCompra
+{
+    //atributos
+    private $itens;
+    private $status;
+    private $valorTotal;
+
+    //métodos
+    public function __construct()
     {
-        return 'ok';
+        $this->itens = [];
+        $this->status = 'aberto';
+        $this->valorTotal = 0;
+    }
+
+    public function exibirItens()
+    {
+        return $this->itens;
+    }
+
+    public function adicionarItem(string $item, float $valor)
+    {
+        array_push($this->itens, ["item" => $item, "valor" => $valor]);
+        $this->valorTotal += $valor;
+        return true;
+    }
+
+    public function exibirValorTotal()
+    {
+        return $this->valorTotal;
+    }
+
+    public function exibirStatus()
+    {
+        return $this->status;
+    }
+
+    public function confirmarPedido()
+    {
+        if ($this->validarCarrinho()) {
+            $this->status = 'confirmado';
+            $this->enviarEmailConfirmacao();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function enviarEmailConfirmacao()
+    {
+        echo '<br/>... envia e-mail de confirmação... <br/>';
+    }
+
+    public function validarCarrinho()
+    {
+        return count($this->itens) > 0;
     }
 }
